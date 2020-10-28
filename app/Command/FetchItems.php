e87d5763bee8859bcfa922dcfbc8f7c19650e643
Brama Udi <bramaudi.al@gmail.com>
2020-10-28 21:22:23 +0700

    force update option

--- app/Command/FetchItems.php
+++ app/Command/FetchItems.php
@@ -5,6 +5,7 @@ namespace App\Command;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class FetchItems extends Command
@@ -14,6 +15,7 @@ class FetchItems extends Command
     protected function configure()
     {
         $this
+        ->addOption('update', 'u', InputOption::VALUE_NONE, 'Update existing data')
         ->setDescription('Make items.txt')
         ->setHelp('This command allow you to make items.txt file contain list of items.');
     }
@@ -22,6 +24,7 @@ class FetchItems extends Command
     {
         ProgressBar::setFormatDefinition('custom', "%percent%% [%bar%] %current%/%max% -- %message%");
         $items = explode("\n", file_get_contents('./items.txt'));
+        $update = $input->getOption('update');
         $progress = new ProgressBar($output, count($items));
         $progress->setFormat('custom');
 
@@ -30,12 +33,13 @@ class FetchItems extends Command
         foreach ($items as $item) {
             $file = str_replace("'", '', $item);
             $file = str_replace("/", '_', $file);
-            if (!file_exists("./data/items/$file.json")) {
+            if (!file_exists("./data/items/$file.json") || $update) {
                 exec("./terraguide.php get:items \"$item\"");
             }
             $progress->advance();
             $progress->setMessage("<fg=cyan>$item</>");
         }
+        $progress->setMessage("<fg=green>Complete!</>\n");
         $progress->finish();
 
         return Command::SUCCESS;
