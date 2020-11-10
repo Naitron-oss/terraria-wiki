db5a1b00c9fd2f4d3b48768652535598ea62ba5f
Brama Udi <bramaudi.al@gmail.com>
2020-11-11 04:43:11 +0700

    remove page, use plain list

--- app/Command/MakePage.php
+++ app/Command/MakePage.php
@@ -20,40 +20,40 @@ class MakePage extends Command
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         ProgressBar::setFormatDefinition('custom', "%percent%% [%bar%] %current%/%max% -- %message%");
-        $json = [];
-        $page = 0;
-        $limit = 20;
+        // $json = [];
+        // $page = 0;
+        // $limit = 20;
         
-        $progress = new ProgressBar($output, count(glob('./data/items/*.json')));
-        $progress->setMessage('Memulai');
-        $progress->start();
-        foreach (glob('./data/items/*.json') as $file) {
-            $title = basename($file, '.json');
-            $json[] = $title;
-            if (count($json) >= $limit) {
-                $page++;
-                file_put_contents("./data/page/$page.json", json_encode($json, JSON_PRETTY_PRINT));
-                $json = [];
-                $progress->setMessage("[<fg=green>Ok</>] $page.json");
-                $progress->advance();
-            }
-        }
-        $progress->finish();
-        $output->writeln("\n[<fg=green>Done</>] Generated <fg=yellow>$page</> pages.");
+        // $progress = new ProgressBar($output, count(glob('./data/items/*.json')));
+        // $progress->setMessage('Memulai');
+        // $progress->start();
+        // foreach (glob('./data/items/*.json') as $file) {
+        //     $title = basename($file, '.json');
+        //     $json[] = $title;
+        //     if (count($json) >= $limit) {
+        //         $page++;
+        //         file_put_contents("./data/page/$page.json", json_encode($json, JSON_PRETTY_PRINT));
+        //         $json = [];
+        //         $progress->setMessage("[<fg=green>Ok</>] $page.json");
+        //         $progress->advance();
+        //     }
+        // }
+        // $progress->finish();
+        // $output->writeln("\n[<fg=green>Done</>] Generated <fg=yellow>$page</> pages.");
 
-        file_put_contents('./data/page/pagination.json', json_encode([
-            'pages' => $page,
-            'limit' => $limit
-        ], JSON_PRETTY_PRINT));
-        $output->writeln("[<fg=green>Ok</>] pagination.json");
+        // file_put_contents('./data/page/pagination.json', json_encode([
+        //     'pages' => $page,
+        //     'limit' => $limit
+        // ], JSON_PRETTY_PRINT));
+        // $output->writeln("[<fg=green>Ok</>] pagination.json");
 
         $json = [];
         foreach (glob('./data/items/*.json') as $file) {
             $title = basename($file, '.json');
             $json[] = $title;
         };
-        file_put_contents('./data/page/all.json', json_encode($json, JSON_PRETTY_PRINT));
-        $output->writeln("[<fg=green>Ok</>] all.json");
+        file_put_contents('./data/items.json', json_encode($json, JSON_PRETTY_PRINT));
+        $output->writeln("[<fg=green>Ok</>] items.json");
 
         return Command::SUCCESS;
     }
